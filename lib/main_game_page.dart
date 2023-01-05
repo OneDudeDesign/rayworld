fdecf1035bbb63e2827b4f27634bbae2ed810d79
Christopher Lucier <OneDudeDesign@gmail.com>
2023-01-05 08:14:46 -0800

    Added Game imports and game widget to state

--- lib/main_game_page.dart
+++ lib/main_game_page.dart
@@ -30,6 +30,9 @@ import 'package:flutter/material.dart';
 import 'helpers/direction.dart';
 import 'helpers/joypad.dart';
 
+import 'package:flame/game.dart';
+import 'ray_world_game.dart';
+
 class MainGamePage extends StatefulWidget {
   const MainGamePage({Key? key}) : super(key: key);
 
@@ -38,13 +41,15 @@ class MainGamePage extends StatefulWidget {
 }
 
 class MainGameState extends State<MainGamePage> {
+  RayWorldGame game = RayWorldGame();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color.fromRGBO(0, 0, 0, 1),
         body: Stack(
           children: [
-            // TODO 1
+            GameWidget(game: game),
             Align(
               alignment: Alignment.bottomRight,
               child: Padding(
