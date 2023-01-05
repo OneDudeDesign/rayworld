68668e1ac648af9c0f335391dea3f7cbde7bac79
Christopher Lucier <OneDudeDesign@gmail.com>
2023-01-05 08:43:18 -0800

    Player Added

--- lib/ray_world_game.dart
+++ lib/ray_world_game.dart
@@ -1,8 +1,11 @@
 import 'package:flame/game.dart';
+import 'components/player.dart';
 
 class RayWorldGame extends FlameGame {
+  final Player _player = Player();
+
   @override
   Future<void> onLoad() async {
-    // empty
+    add(_player);
   }
 }
